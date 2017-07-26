209bb376c794981681916d59a366db205deb90c3
DataPacRat <datapacrat@gmail.com>
2017-07-26 03:56:29 -0400

    Update hello.f90

--- hello.f90
+++ hello.f90
@@ -1,5 +1,4 @@
 program hello
-    implicit none
 !
 ! Title: Hello World
 ! Authour: DataPacRat <datapacrat@datapacrat.com>
@@ -11,5 +10,33 @@ program hello
 ! I plan on using it as a template to throw in better coding practices
 ! as I learn them.
 !
-    print *, "Hello World"
+    implicit none
+    ! Without the previous line, Fortran has a weird default behaviour:
+    ! variables starting with i through n are integers, the rest reals.
+    real :: a, b, c
+    integer :: j, k, l
+    complex :: euler
+    real, parameter :: pi = 4.0 * atan(1.0)
+    ! parameters can't be changed after initial assignment.
+    real, parameter :: phi = (1 + (5 ** 0.5)) / 2
+    real, parameter :: e = exp(1.0)
+    complex, parameter :: i = (0,1) ! sqrt(-1)
+    character (len = 11) :: word ! word holds 11 characters
+    word = "Hello World"
+    write (*,*) "Hello World"
+    write(*,*) "pi: ",pi
+    write(*,*) "phi: ",phi
+    write(*,*) "e: ",e
+    write(*,*) "i: ",i
+  euler = (e ** (pi * i)) + 1
+    write(*,*) "e^(pi*i)+1:", euler
+    if (euler == (0.0,0.0)) then
+        write(*,*) 'Math works :)'
+    elseif (euler /= (0.0,0.0)) then
+        write(*,*) "Looks like math doesn't work. :("
+    else
+        write(*,*) "Looks like logic doesn't work either. :("
+    endif
+    write(*,*) word
+    stop "Goodbye World"
 end program hello
