27a584aa89856bcf14c21969cb26dcdaea07fedf
DataPacRat <datapacrat@gmail.com>
2017-07-25 21:06:37 -0400

    Create hello.f90

---
+++ hello.f90
@@ -0,0 +1,15 @@
+program hello
+    implicit none
+!
+! Title: Hello World
+! Authour: DataPacRat <datapacrat@datapacrat.com>
+! Date: 2017/07/25
+! License: Modified BSD License
+!
+! Traditional teaching program to demonstrate basic form.
+!
+! I plan on using it as a template to throw in better coding practices
+! as I learn them.
+!
+    print *, "Hello World"
+end program hello
