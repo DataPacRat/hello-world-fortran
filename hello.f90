884ce82282f5a2754a661eff339e450726ccb7f2
DataPacRat <datapacrat@gmail.com>
2017-07-26 04:48:23 -0400

    Update hello.f90

--- hello.f90
+++ hello.f90
@@ -2,7 +2,7 @@ program hello
 !
 ! Title: Hello World
 ! Authour: DataPacRat <datapacrat@datapacrat.com>
-! Date: 2017/07/25
+! Date: 2017/07/26
 ! License: Modified BSD License, aka BSD-3-Clause
 !
 ! Traditional teaching program to demonstrate basic form.
@@ -18,7 +18,7 @@ program hello
     complex :: euler
     real, parameter :: pi = 4.0 * atan(1.0)
     ! parameters can't be changed after initial assignment.
-    real, parameter :: phi = (1 + (5 ** 0.5)) / 2
+    real, parameter :: phi = (1.0 + (5.0 ** 0.5)) / 2.0
     real, parameter :: e = exp(1.0)
     complex, parameter :: i = (0,1) ! sqrt(-1)
     character (len = 11) :: word ! word holds 11 characters
@@ -28,7 +28,7 @@ program hello
     write(*,*) "phi: ",phi
     write(*,*) "e: ",e
     write(*,*) "i: ",i
-    euler = (e ** (pi * i)) + 1
+    euler = (e ** (pi * i)) + 1.0
     write(*,*) "e^(pi*i)+1:", euler
     if (euler == (0.0,0.0)) then
         write(*,*) 'Math works :)'
